b97f19feb4e3945d316a37fd0259392752995425
KIKA003 <ch.gal2014@gmail.com>
2024-06-19 11:01:18 +0000

    aggiunta foto praga

--- index.js
+++ index.js
@@ -22,14 +22,24 @@ let images = [
         src: "images/about.jpg"
     },
     {
-        name: "foto",
+        name: "praga",
         src: "images/IMG_6423.JPG"
+    },
+    {
+        name: "praga2",
+        src: "images/IMG_4616.jpg"
+    },
+    {
+        name: "praga3",
+        src: "images/IMG_4890.jpg"
     },
 ]
 
 document.getElementById("chiara").src = images[0].src;
 document.getElementById("chiara2").src = images[1].src;
-document.getElementById("foto").src = images[2].src;
+document.getElementById("praga").src = images[2].src;
+document.getElementById("praga2").src = images[3].src;
+document.getElementById("praga3").src = images[3].src;
 
 
 
