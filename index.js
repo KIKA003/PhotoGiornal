2639259fd6510f6dfbef5f5f20abc2b09fb510e6
KIKA003 <ch.gal2014@gmail.com>
2024-07-01 23:12:04 +0000

    penombra carimento foto

--- index.js
+++ index.js
@@ -43,11 +43,63 @@ let penombra = [
     { 
         name: "penombra2",
         src: "penombra/IMG_6641-2-2.jpg"
+    },
+    { 
+        name: "penombra3",
+        src: "penombra/IMG_6664-2.jpg"
+    },   
+    { 
+        name: "penombra4",
+        src: "penombra/IMG_6670-2-3.jpg"
+    }, 
+    { 
+        name: "penombra5",
+        src: "penombra/IMG_6653-2.jpg"
+    },  
+    { 
+        name: "penombra6",
+        src: "penombra/IMG_6682-2.jpg"
+    },
+    { 
+        name: "penombra7",
+        src: "penombra/IMG_6825-3.jpg"
+    },  
+    { 
+        name: "penombra8",
+        src: "penombra/IMG_6656-2.jpg"
+    },    
+    { 
+        name: "penombra9",
+        src: "penombra/IMG_6800-2.jpg"
+    },  
+    { 
+        name: "penombra10",
+        src: "penombra/IMG_6854-2.jpg"
+    },
+    { 
+        name: "penombra11",
+        src: "penombra/IMG_6830-3.jpg"
+    },  
+    { 
+        name: "penombra12",
+        src: "penombra/IMG_6755-2.jpg"
     },    
 ]
 
+
 document.getElementById("penombra1").src = penombra[0].src;
 document.getElementById ("penombra2"). src = penombra[1].src;
+document.getElementById ("penombra3"). src = penombra[2].src;
+document.getElementById ("penombra4"). src = penombra[3].src;
+document.getElementById ("penombra5"). src = penombra[4].src;
+document.getElementById ("penombra6"). src = penombra[5].src;
+document.getElementById ("penombra7"). src = penombra[6].src;
+document.getElementById ("penombra8"). src = penombra[7].src;
+document.getElementById ("penombra9"). src = penombra[8].src;
+document.getElementById ("penombra10"). src = penombra[9].src;
+document.getElementById ("penombra11"). src = penombra[10].src;
+document.getElementById ("penombra12"). src = penombra[11].src;
+
 document.getElementById("praga").src = images[0].src;
 document.getElementById("praga2").src = images[1].src;
 document.getElementById("praga3").src = images[2].src;
